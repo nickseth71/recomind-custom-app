c140cee37145abcda83e513c4c9d576be0ef784c
Tayyab-Raza <traza9192@gmail.com>
2026-09-17 17:48:16 +0530

    fix: webhook

--- app/routes/webhooks.app.uninstalled.jsx
+++ app/routes/webhooks.app.uninstalled.jsx
@@ -6,11 +6,9 @@ export const action = async ({ request }) => {
 
   console.log(`Received ${topic} webhook for ${shop}`);
 
-  // Webhook requests can trigger multiple times and after an app has already been uninstalled.
-  // If this webhook already ran, the session may have been deleted previously.
-  if (session) {
-    await db.session.deleteMany({ where: { shop } });
-  }
+  // Webhook requests can be retried after the records were already deleted.
+  await db.session.deleteMany({ where: { shop } });
+  await db.jwt.deleteMany({ where: { recomind_shop: shop } });
 
   return new Response();
 };
